ac0eeb0e4d8bebb40db866fee3621c230692994f
louna <r.arswendo.r@gmail.com>
2024-10-13 14:51:40 +0700

    WIP: UDP communication

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
 func main() {
-	fmt.Println("Server started")
+	udpAddr := net.UDPAddr{
+		Port: 6969,
+		IP:   net.ParseIP("127.0.0.1"),
+	}
+	listener, err := net.ListenUDP("udp", &udpAddr)
+	if err != nil {
+		panic(err)
+	}
+
+	packet := make([]byte, 64)
+	fmt.Println("UDP Listening on 6969")
+	for {
+		n, remoteAddress, err := listener.ReadFromUDP(packet)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf(
+			"Message from: %v, of length: %v, Containing: %s\n",
+			remoteAddress,
+			n,
+			string(packet),
+		)
+		_, err = listener.WriteToUDP([]byte("Hey ;)"), remoteAddress)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
