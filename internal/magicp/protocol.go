5fc1cbdf59dab6cf2041a3b3d6a1932f23ae79e6
louna <r.arswendo.r@gmail.com>
2024-10-14 22:07:01 +0700

    WIP: plan for protocol

---
+++ internal/magicp/protocol.go
@@ -0,0 +1,13 @@
+package magicp
+
+// What the client need to transfer
+// 1. Their position (SNAPSHOT OF THE WORLD)
+// 2. Each bullets starting position they fire (SNAPSHOT OF THE WORLD)
+// 3. Their intent (moving, shooting, etc.)
+
+// What the server need to inform
+// 1. Each of the players position
+// 2. Each bullets position on the field
+// 3. If the game is over (eg: player got hit)
+
+// Client Request Protocol:
